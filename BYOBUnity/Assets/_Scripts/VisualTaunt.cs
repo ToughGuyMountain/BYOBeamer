16fe55176c0fb01b600ee33e66efe84cee3ca866
Allan Lavell <alavell@gmail.com>
2014-06-01 13:39:49 -0400

    Basic visual taunt setup

---
+++ BYOBUnity/Assets/_Scripts/VisualTaunt.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class VisualTaunt : MonoBehaviour {
+	public GameObject[] tauntPrefabs0;
+	public GameObject[] tauntPrefabs1;
+	int index0;
+	int index1;
+	
+	void Update() {
+		var inputDevice = InputManager.ActiveDevice;
+		
+		if (inputDevice.LeftTrigger) {
+			index0 = (index0 + 1) % tauntPrefabs0.Length;
+			Instantiate(tauntPrefabs0[index0], transform.position, Quaternion.identity);
+		}
+	}
+}
