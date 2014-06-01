ff2e1a98614db61778d65973c1261235c140ff1f
Allan Lavell <alavell@gmail.com>
2014-06-01 12:01:12 -0400

    Basic approximation of macbook speekz in the middle of a pyramid

---
+++ BYOBUnity/Assets/_Scripts/RotateAtSpeed.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateAtSpeed : MonoBehaviour {
+	public float speed = 1.0f;
+	public bool cw = true;
+	public Vector3 axisOfRotation;
+	float angle;
+	public bool randomStartingOffset;
+	
+	void Start() {
+		if (randomStartingOffset) {
+			angle += UnityEngine.Random.value * 360;
+		}
+	}
+	
+	void Update() {
+		angle += cw ? speed * Time.deltaTime : -speed * Time.deltaTime;
+		transform.localRotation = Quaternion.AngleAxis(angle, axisOfRotation);
+	}	
+}
