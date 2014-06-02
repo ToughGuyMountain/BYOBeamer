ff91fcf2cc21a6fafd619b77d454100c11e9597f
Allan Lavell <alavell@gmail.com>
2014-06-02 10:59:04 -0400

    Add compy playing movies

---
+++ BYOBUnity/Assets/_Scripts/PlayMovie.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayMovie : MonoBehaviour {
+	
+	void Start () {
+		var movTexture = renderer.material.mainTexture as MovieTexture;
+		movTexture.loop = true;
+		movTexture.Play();
+	}
+
+}
