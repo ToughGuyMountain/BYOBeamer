27729e130d823de79e75f351a069f92b8f06a732
Catherine Bluemke <catbluemke@gmail.com>
2014-06-04 12:01:43 -0400

    weird interns add

--- BYOBUnity/Assets/CoolJump.cs
+++ BYOBUnity/Assets/CoolJump.cs
@@ -11,12 +11,22 @@ public class CoolJump : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
+		NoDeath ();
 	}
 
 	void OnCollisionEnter (Collision info) {
-		Debug.Log ("colliz");
+
 		rigidbody.AddForce (info.contacts[0].normal * (200 + UnityEngine.Random.value * 200), ForceMode.Acceleration);
+
+	}
+
+	void NoDeath () {
+
+		if (Mathf.Approximately(rigidbody.velocity.magnitude, 0)) {
+
+			rigidbody.AddForce (Vector3.up * 50, ForceMode.Acceleration);
+
+		}
 
 	}
 }
