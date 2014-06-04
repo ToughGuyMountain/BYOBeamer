69eecc1c6dd80d02aecbbc8eef0855b1719e9923
Catherine Bluemke <catbluemke@gmail.com>
2014-06-04 03:23:39 -0400

    Intern Pergatory work + jumping water coolers yeeeeah

---
+++ BYOBUnity/Assets/CoolJump.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoolJump : MonoBehaviour {
+	
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter (Collision info) {
+		Debug.Log ("colliz");
+		rigidbody.AddForce (info.contacts[0].normal * (200 + UnityEngine.Random.value * 200), ForceMode.Acceleration);
+
+	}
+}
